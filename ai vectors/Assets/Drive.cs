d65c350f91d3bdddcf70551524348cf90938b73c
715018 <715018@student.dusd.net>
2023-12-07 17:59:44 -0800

    Revert "Merge branch 'main' of https://github.com/715018/mathai"
    This reverts commit 7d62f8f08c65aae3bf8a9aa89a256c3eb4d90183, reversing
    changes made to 216fd845a56213a95410c0d00c2f9a347d36114b.

--- ai vectors/Assets/Drive.cs
+++ ai vectors/Assets/Drive.cs
@@ -9,50 +9,21 @@ public class Drive : MonoBehaviour
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
     public GameObject fuel;
-    bool autopilot = false;
-    float tspeed = 5f;
-    float rspeed = .1f;
+
     void Start()
     {
 
-    }
-
-    void AutoPilot()
-    {
-        CalculateAngle();
-        this.transform.position += transform.up * tspeed * Time.deltaTime;
     }
     void CalculateAngle()
     {
         Vector3 tankForward = transform.up;
         Vector3 fuelDirection = fuel.transform.position - transform.position;
 
-       Debug.DrawRay(this.transform.position, tankForward * 10, Color.green, 5);
-        Debug.DrawRay(this.transform.position, fuelDirection, Color.red, 5);
-
-        float dot = (tankForward.x * fuelDirection.x + tankForward.y * fuelDirection.y);
-        float angle = Mathf.Acos(dot / (tankForward.magnitude * fuelDirection.magnitude));
-        Debug.Log("Angle: " + angle * Mathf.Rad2Deg);
-        Debug.Log("Unity Angle: " + Vector3.Angle(tankForward, fuelDirection));
-
-        int clockwise = 1;
-        if (Cross(tankForward, fuelDirection).z < 0)
-            clockwise = -1;
-
-        if((angle * Mathf.Rad2Deg )> 10)
-
-        this.transform.Rotate(0, 0, angle * Mathf.Rad2Deg * clockwise * rspeed);
+        Debug.DrawRay(this.transform.position, tankForward, Color.green, 10);
+        Debug.DrawRay(this.transform.position, fuelDirection, Color.red, 10);
     }
 
-         Vector3 Cross(Vector3 v,  Vector3 w)
-         {
-            float xMult = v.y * w.z - v.z * w.y;
-            float yMult = v.x * w.z - v.z * w.x;
-            float zMult = v.x * w.y - v.y * w.x;
-
-            return (new Vector3(xMult, yMult, zMult));
-         }
-    float  CalculateDistance()
+    void CalculateDistance()
     {
         float distance = Mathf.Sqrt(Mathf.Pow(fuel.transform.position.x - transform.position.x,2) +
             Mathf.Pow(fuel.transform.position.z - transform.position.z,2));
@@ -68,8 +39,6 @@ public class Drive : MonoBehaviour
         Debug.Log("U Distance: " +  uDistance);
         Debug.Log("V Magnitude: " + tankToFuel.magnitude);
         Debug.Log("V sqrMagnitude: " + tankToFuel.sqrMagnitude);
-
-        return distance;
     }
     void LateUpdate()
     {
@@ -94,17 +63,5 @@ public class Drive : MonoBehaviour
             CalculateDistance();
             CalculateAngle();
         }
-       
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            autopilot = !autopilot;
-        }
-        
-        if (CalculateDistance() < 3)
-            autopilot = false;
-
-        if (autopilot) 
-            AutoPilot();
-
     }
 }
